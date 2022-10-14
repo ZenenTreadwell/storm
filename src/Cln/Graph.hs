c96cff35c4bee7e4fca8b17272d4d59acde05f09
Your Name <you@example.com>
2022-10-13 19:20:31 -0700

    nof

--- src/Cln/Graph.hs
+++ src/Cln/Graph.hs
@@ -38,12 +38,15 @@ loadGraph = (allchannels) >>= \case
     (Just (Correct (Res listchannels _))) -> (allnodes) >>= \case 
         (Just (Correct (Res listnodes _))) -> do 
             liftIO $ writeIORef graphRef
-                   -- $ (\g -> (flip subgraph g) . head $ components g )
-                   -- $ (\g -> foldr (delEdge.toEdge) g $ filter (feeTooHigh.edgeLabel) (labEdges g) ) 
-                   -- $ gfiltermap traps
                    $ mkGraph (map toLNode nx) (map toLEdge' cx)
                    where cx = (channels::ListChannels->[Channel]) listchannels 
                          nx = (_nodes :: ListNodes -> [NodeInfo]) listnodes
+                         toLNode ni = ( (getNodeInt.nodeid) ni , ni)
+                         toLEdge' c = (
+                           ( getNodeInt.source) c
+                           , (getNodeInt.(destination::Channel->String)) c
+                           , c
+                           )
         otherwise -> pure () 
     otherwise -> pure () 
 
@@ -51,26 +54,3 @@ getNodeInt :: String -> Node
 getNodeInt s = case readHex.filter isHexDigit $ s of 
     ([]) -> 0 
     (x:_)-> fst x 
-
-toLNode :: NodeInfo -> LNode NodeInfo 
-toLNode ni = ( (getNodeInt.nodeid) ni , ni)
-
-toLEdge' :: Channel -> LEdge Channel
-toLEdge' c = (
-      (getNodeInt.source) c
-    , (getNodeInt.(destination::Channel->String)) c
-    , c )
-
-feeTooHigh :: Channel -> Bool 
-feeTooHigh c = (||) 
-    (base_fee_millisatoshi c > 111111) 
-    (fee_per_millionth c > 11111)
-
-traps :: Cxt -> Maybe Cxt  
-traps c =
-    let inny = (indeg' c) == 0 
-        outy = (outdeg' c) == 0
-    in if (inny || outy)   
-        then Nothing 
-        else Just c
-
